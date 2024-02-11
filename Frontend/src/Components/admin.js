607b7ff8e29b79f532d4cafe58b1ad12e773774e
coffee-loves-code-2003 <128959041+coffee-loves-code-2003@users.noreply.github.com>
2024-02-11 09:20:31 +0530

    Latest Changes(ADMIN-11-02-24)

--- Frontend/src/Components/admin.js
+++ Frontend/src/Components/admin.js
@@ -12,12 +12,14 @@ import {  useSelector } from "react-redux";
     <h2 className='text-center my-5'>Coordinator Page</h2>
    <p style={{color:'black'}} className='text-center'>Welcome to Coordinator page. You can download the most recent User registration sheets here. Do not share your credentials.</p>
 
-<div className='row text-center'>
+<div className='row text-center mb-5'>
 <div className='col'>
 View All Registration Sheets on Google Drive
 <br></br><br></br>
 <br></br>
-<a href='https://drive.google.com/drive/folders/1dLONLk3XPDfeBMtSVyrwONpUHbjhQL0o?usp=sharing' target='_blank' style={{backgroundColor:'purple',padding:'10px',borderRadius:'8px', textDecoration:'none',color:'white',fontSize:'17px'}}>View all sheets</a>
+<br></br><iframe src="https://drive.google.com/embeddedfolderview?id=1dLONLk3XPDfeBMtSVyrwONpUHbjhQL0o" frameborder="0" style={{width:"80%",height:"250px"}}>Google Drive Embed</iframe>
+<br></br><a href='https://drive.google.com/drive/folders/1dLONLk3XPDfeBMtSVyrwONpUHbjhQL0o?usp=sharing' target='_blank' style={{backgroundColor:'purple',padding:'10px',borderRadius:'8px', textDecoration:'none',color:'white',fontSize:'17px'}}>Open in Google Drive</a>
+
 </div>
 
 </div>
@@ -31,4 +33,3 @@ View All Registration Sheets on Google Drive
 };
 
 export default Admin;
-
